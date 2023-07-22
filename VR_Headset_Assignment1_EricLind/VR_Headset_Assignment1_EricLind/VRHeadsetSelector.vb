adfaf661877ad5a0512bcd866c1927da1a47bf79
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-07-22 14:26:55 -0700

    Week 1 - P2

--- VR_Headset_Assignment1_EricLind/VR_Headset_Assignment1_EricLind/VRHeadsetSelector.vb
+++ VR_Headset_Assignment1_EricLind/VR_Headset_Assignment1_EricLind/VRHeadsetSelector.vb
@@ -1,5 +1,9 @@
 ﻿Public Class VRHeadsetSelector
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub VRHeadSetSelector_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
 End Class
