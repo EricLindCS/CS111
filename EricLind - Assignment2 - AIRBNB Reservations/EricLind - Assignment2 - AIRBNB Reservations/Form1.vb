8f3a130e5603713e93624f38e60872f119d55f0b
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-07-29 16:55:22 -0700

    Week2-1

---
+++ EricLind - Assignment2 - AIRBNB Reservations/EricLind - Assignment2 - AIRBNB Reservations/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmAirBnB
+    Private Sub appPic_Click(sender As Object, e As EventArgs) Handles appPic.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles titleLbl.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
