2d2615d01ed6f0d6f1d1c79e906bd1a3e9b67182
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-07-29 19:50:24 -0700

    W2-final-v1

--- EricLind - Assignment 2 - AirBnB Reservation/EricLind - Assignment 2 - AirBnB Reservation/frmAirBnBRes.Designer.vb
+++ EricLind - Assignment 2 - AirBnB Reservation/EricLind - Assignment 2 - AirBnB Reservation/frmAirBnBRes.Designer.vb
@@ -23,79 +23,88 @@ Partial Class frmAirBnbRes
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(frmAirBnbRes))
-        Label1 = New Label()
-        Label2 = New Label()
-        Label3 = New Label()
-        Label4 = New Label()
-        Button1 = New Button()
-        Button2 = New Button()
-        Button3 = New Button()
+        lblFormTitle = New Label()
+        lblFormSubTitle = New Label()
+        lblNumNightsDisp = New Label()
+        lblCostStayDisp = New Label()
+        btnDisp = New Button()
+        btnClear = New Button()
+        btnExit = New Button()
         PictureBox1 = New PictureBox()
+        txtNumNights = New TextBox()
+        lblTotCost = New Label()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
-        ' Label1
+        ' lblFormTitle
         ' 
-        Label1.AutoSize = True
-        Label1.Location = New Point(561, 27)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(41, 15)
-        Label1.TabIndex = 0
-        Label1.Text = "Label1"
+        lblFormTitle.AutoSize = True
+        lblFormTitle.Font = New Font("Cooper Black", 24.0F, FontStyle.Regular, GraphicsUnit.Point)
+        lblFormTitle.Location = New Point(454, 26)
+        lblFormTitle.Name = "lblFormTitle"
+        lblFormTitle.Size = New Size(276, 36)
+        lblFormTitle.TabIndex = 0
+        lblFormTitle.Text = "Orlando AirBnB"
+        lblFormTitle.TextAlign = ContentAlignment.MiddleCenter
         ' 
-        ' Label2
+        ' lblFormSubTitle
         ' 
-        Label2.AutoSize = True
-        Label2.Location = New Point(616, 95)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(41, 15)
-        Label2.TabIndex = 1
-        Label2.Text = "Label2"
+        lblFormSubTitle.AutoSize = True
+        lblFormSubTitle.Font = New Font("Cooper Black", 14.25F, FontStyle.Italic, GraphicsUnit.Point)
+        lblFormSubTitle.Location = New Point(475, 74)
+        lblFormSubTitle.Name = "lblFormSubTitle"
+        lblFormSubTitle.Size = New Size(235, 21)
+        lblFormSubTitle.TabIndex = 1
+        lblFormSubTitle.Text = "Only $XX.XX per night"
         ' 
-        ' Label3
+        ' lblNumNightsDisp
         ' 
-        Label3.AutoSize = True
-        Label3.Location = New Point(493, 149)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(41, 15)
-        Label3.TabIndex = 2
-        Label3.Text = "Label3"
+        lblNumNightsDisp.AutoSize = True
+        lblNumNightsDisp.Font = New Font("Century Gothic", 12.0F, FontStyle.Regular, GraphicsUnit.Point)
+        lblNumNightsDisp.Location = New Point(427, 154)
+        lblNumNightsDisp.Name = "lblNumNightsDisp"
+        lblNumNightsDisp.Size = New Size(149, 21)
+        lblNumNightsDisp.TabIndex = 2
+        lblNumNightsDisp.Text = "Number of Nights:"
+        lblNumNightsDisp.TextAlign = ContentAlignment.MiddleRight
         ' 
-        ' Label4
+        ' lblCostStayDisp
         ' 
-        Label4.AutoSize = True
-        Label4.Location = New Point(506, 188)
-        Label4.Name = "Label4"
-        Label4.Size = New Size(41, 15)
-        Label4.TabIndex = 3
-        Label4.Text = "Label4"
+        lblCostStayDisp.AutoSize = True
+        lblCostStayDisp.Font = New Font("Century Gothic", 12.0F, FontStyle.Regular, GraphicsUnit.Point)
+        lblCostStayDisp.Location = New Point(469, 213)
+        lblCostStayDisp.Name = "lblCostStayDisp"
+        lblCostStayDisp.Size = New Size(107, 21)
+        lblCostStayDisp.TabIndex = 3
+        lblCostStayDisp.Text = "Cost of Stay:"
+        lblCostStayDisp.TextAlign = ContentAlignment.MiddleRight
         ' 
-        ' Button1
+        ' btnDisp
         ' 
-        Button1.Location = New Point(487, 306)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(75, 23)
-        Button1.TabIndex = 4
-        Button1.Text = "Button1"
-        Button1.UseVisualStyleBackColor = True
+        btnDisp.Location = New Point(420, 339)
+        btnDisp.Name = "btnDisp"
+        btnDisp.Size = New Size(105, 35)
+        btnDisp.TabIndex = 4
+        btnDisp.Text = "Display Cost"
+        btnDisp.UseVisualStyleBackColor = True
         ' 
-        ' Button2
+        ' btnClear
         ' 
-        Button2.Location = New Point(576, 310)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(75, 23)
-        Button2.TabIndex = 5
-        Button2.Text = "Button2"
-        Button2.UseVisualStyleBackColor = True
+        btnClear.Location = New Point(531, 339)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(105, 35)
+        btnClear.TabIndex = 5
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
         ' 
-        ' Button3
+        ' btnExit
         ' 
-        Button3.Location = New Point(678, 318)
-        Button3.Name = "Button3"
-        Button3.Size = New Size(75, 23)
-        Button3.TabIndex = 6
-        Button3.Text = "Button3"
-        Button3.UseVisualStyleBackColor = True
+        btnExit.Location = New Point(642, 339)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(105, 35)
+        btnExit.TabIndex = 6
+        btnExit.Text = "Exit"
+        btnExit.UseVisualStyleBackColor = True
         ' 
         ' PictureBox1
         ' 
@@ -107,20 +116,44 @@ Partial Class frmAirBnbRes
         PictureBox1.TabIndex = 7
         PictureBox1.TabStop = False
         ' 
+        ' txtNumNights
+        ' 
+        txtNumNights.Font = New Font("Century Gothic", 12.0F, FontStyle.Regular, GraphicsUnit.Point)
+        txtNumNights.Location = New Point(659, 156)
+        txtNumNights.Name = "txtNumNights"
+        txtNumNights.Size = New Size(41, 27)
+        txtNumNights.TabIndex = 8
+        txtNumNights.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' lblTotCost
+        ' 
+        lblTotCost.AutoSize = True
+        lblTotCost.Font = New Font("Century Gothic", 12.0F, FontStyle.Regular, GraphicsUnit.Point)
+        lblTotCost.Location = New Point(623, 213)
+        lblTotCost.Name = "lblTotCost"
+        lblTotCost.Size = New Size(77, 21)
+        lblTotCost.TabIndex = 9
+        lblTotCost.Text = "$9999.99"
+        lblTotCost.TextAlign = ContentAlignment.MiddleRight
+        ' 
         ' frmAirBnbRes
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AcceptButton = btnDisp
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.Gold
+        CancelButton = btnClear
         ClientSize = New Size(800, 450)
+        Controls.Add(lblTotCost)
+        Controls.Add(txtNumNights)
         Controls.Add(PictureBox1)
-        Controls.Add(Button3)
-        Controls.Add(Button2)
-        Controls.Add(Button1)
-        Controls.Add(Label4)
-        Controls.Add(Label3)
-        Controls.Add(Label2)
-        Controls.Add(Label1)
+        Controls.Add(btnExit)
+        Controls.Add(btnClear)
+        Controls.Add(btnDisp)
+        Controls.Add(lblCostStayDisp)
+        Controls.Add(lblNumNightsDisp)
+        Controls.Add(lblFormSubTitle)
+        Controls.Add(lblFormTitle)
         Name = "frmAirBnbRes"
         Text = "AirBnB Reservations"
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
@@ -128,12 +161,14 @@ Partial Class frmAirBnbRes
         PerformLayout()
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents lblFormTitle As Label
+    Friend WithEvents lblFormSubTitle As Label
+    Friend WithEvents lblNumNightsDisp As Label
+    Friend WithEvents lblCostStayDisp As Label
+    Friend WithEvents btnDisp As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtNumNights As TextBox
+    Friend WithEvents lblTotCost As Label
 End Class
--- EricLind - Assignment 2 - AirBnB Reservation/EricLind - Assignment 2 - AirBnB Reservation/frmAirBnBRes.vb
+++ EricLind - Assignment 2 - AirBnB Reservation/EricLind - Assignment 2 - AirBnB Reservation/frmAirBnBRes.vb
@@ -5,15 +5,33 @@ Public Class frmAirBnbRes
 
     Const _airBnbPricePerDay As Double = 79.99
 
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-        ' Dim strNumNights As String
-        'Dim intNumNights As Integer
-        ' Dim decTotalCost As Double
-        '
-        ' strNumNights = txtNumNights.Text
-        ' intNumNights = Convert.ToInt32(strNumNights)
-        'decTotalCost = intNumNights * _airBnbPricePerDay
-        'lblTotalCost.txt = decTotalCost.ToString("C")
+    Private Sub btnDisp_Click(sender As Object, e As EventArgs) Handles btnDisp.Click
+        Dim strNumNights As String
+        Dim intNumNights As Integer
+        Dim decTotalCost As Double
+
+        strNumNights = txtNumNights.Text
+        intNumNights = Convert.ToInt32(strNumNights)
+        decTotalCost = intNumNights * _airBnbPricePerDay
+        lblTotCost.Text = decTotalCost.ToString("C")
 
     End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtNumNights.Clear()
+        lblTotCost.Text = ""
+        txtNumNights.Focus()
+
+    End Sub
+
+    Private Sub frmAirBnbRes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblFormSubTitle.Text = "Only " & _airBnbPricePerDay & " per night"
+        lblTotCost.Text = ""
+        txtNumNights.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
 End Class
