9ddaf86fb0aa47605be2d7ca2e86ef4b8647dfb2
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-07-29 19:23:33 -0700

    W2-3

--- EricLind - Assignment 2 - AirBnB Reservation/EricLind - Assignment 2 - AirBnB Reservation/frmAirBnBRes.vb
+++ EricLind - Assignment 2 - AirBnB Reservation/EricLind - Assignment 2 - AirBnB Reservation/frmAirBnBRes.vb
@@ -1,5 +1,19 @@
-﻿Public Class frmAirBnbRes
+﻿'Eric Lind
+
+Option Strict On
+Public Class frmAirBnbRes
+
+    Const _airBnbPricePerDay As Double = 79.99
+
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        ' Dim strNumNights As String
+        'Dim intNumNights As Integer
+        ' Dim decTotalCost As Double
+        '
+        ' strNumNights = txtNumNights.Text
+        ' intNumNights = Convert.ToInt32(strNumNights)
+        'decTotalCost = intNumNights * _airBnbPricePerDay
+        'lblTotalCost.txt = decTotalCost.ToString("C")
 
     End Sub
 End Class
