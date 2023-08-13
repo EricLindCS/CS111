9bd4fe92c00fa99170db332fe09e7e5315992bfa
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-08-12 20:20:53 -0700

    Cleanup

--- VR_Headset_Assignment1_EricLind/VR_Headset_Assignment1_EricLind/VRHeadsetSelector.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class VRHeadsetSelector
-    Private Sub VRHeadSetSelector_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-End Class
